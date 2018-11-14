4794dd5d0dbaf37886d3c41e73c1105137ed3f46
zpellman <zpellman@gmail.com>
2018-11-14 11:59:23 -0700

    Create virtualenv.sh
    Add Python virtualenvwrapper exports and startup file

---
+++ .bashrc.d/virtualenv.sh
@@ -0,0 +1,5 @@
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Documents/Projects/
+
+source $HOME/.local/bin/virtualenvwrapper.sh 2>/dev/null
